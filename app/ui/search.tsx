e2adde8d3c56511ab825a955d9f8d30ac50088ad
ay981 <aymenabdulkerim411@gmail.com>
2026-03-10 15:37:06 +0300

    fix: ensure latest invoices default to an empty array and refactor revenue chart data handling

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-    const searchParams = useSearchParams();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
@@ -13,7 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('search');
     }
-    console.log(term);
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
